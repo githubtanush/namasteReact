c9754017302a2566abd2ad9201a62fc846d2dafa
Tanush Arora <tanush935@gmail.com>
2025-10-02 12:12:58 +0530

    inception commit

---
+++ Chapter-01-Inception/app.js
@@ -0,0 +1,35 @@
+// const heading = React.createElement("h1",{id:"heading"},"Hello world from React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+/*
+ <div id = "parent">
+    <div id = "child">
+    <h1> I'm h1 tag </h1>
+    </div>
+</div>
+*/
+
+
+//more complex if we do insert one more child
+const parent = React.createElement("div",{id:"parent"},[
+    React.createElement(
+        "div",
+        {id : "child"},[
+            React.createElement("h1",{},"I'm an h1 tag"),
+            React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+    React.createElement(
+        "div",
+        {id : "child"},[
+            React.createElement("h1",{},"I'm an h1 tag"),
+            React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+]);
+
+//JSX
+
+console.log("parent") // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
