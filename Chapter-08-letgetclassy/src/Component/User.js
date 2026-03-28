40935c3e07eb40ea03604d36f3722c15a10541ba
Tanush Arora <tanush935@gmail.com>
2026-03-28 19:07:08 +0530

    2 chapters completed

--- Chapter-08-letgetclassy/src/Component/User.js
+++
@@ -1,8 +0,0 @@
-const User = () => {
-    return <div className="user-card">
-        <h2>Name : Tanush</h2>
-        <h3>Location : Ludhiana</h3>
-        <h4>Contact : tanush935@gmail.com</h4>
-    </div>
-}
-export default User;
